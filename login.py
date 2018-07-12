2a58f9aae200c0b002f5108a8c242bddc3c5e6cd
Eric-Thomas <35507043+Eric-Thomas@users.noreply.github.com>
2018-07-12 12:42:32 +0000

    Delete login.py

--- login.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import time
-driver = webdriver.Edge()
-driver.get("https://itsm.fenetwork.com/HPSM9.33_PROD/index.do")
-time.sleep(2)
-driver.find_element_by_id("LoginUsername").send_keys("55489")
-driver.find_element_by_id("LoginPassword").send_keys("First internship27")
-driver.find_element_by_id("loginBtn").click()
-
