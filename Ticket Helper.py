9a0ce8dde9d556eba559088ba59aa8a2ff749ff1
Eric-Thomas <35507043+Eric-Thomas@users.noreply.github.com>
2018-07-13 09:50:54 -0400

    Add files via upload

--- Ticket Helper.py
+++ Ticket Helper.py
@@ -17,10 +17,13 @@ try:
 	import getpass
 except (ImportError):
 	sys.exit("Error importing getpass module")
+# Prompts user for name of excel file and creates that file with a worksheet named "PI Requests"
+# A header is added for all the info needed for PI software requests
+# Returns a dictionary is returned with workbook and worksheet objects
 def create_excel_file():
 	# Ask user for name of file
 	print("Enter full path name for excel file including .xlsx extension\nIf full path isn't entered file will save in same directory as the script\n>>", end="")
-	fileName = input()
+	fileName = str(input())
 	spreadsheet = {}
 	try:
 		# Create workbook
@@ -34,7 +37,7 @@ def create_excel_file():
 		return spreadsheet
 	except (OSError):
 		sys.exit("Error creating excel file")
-# Creates and formats first row of spreadsheet
+# Creates and formats first row of spreadsheet with necessary info for PI software requests
 def create_header(workbook, worksheet):
 	# Create cell format
 	cellFormat = workbook.add_format()
@@ -48,11 +51,14 @@ def create_header(workbook, worksheet):
 	for i in range(len(headerText)):
 		worksheet.set_column(i, i, widths[i])
 		worksheet.write_string(0, i, headerText[i], cellFormat)
+# Prompts user for username and password which is invisible to user
+# Returns a dictionary with the username and password
 def collect_login_info():
 	print("Enter username for HP Service Manager\n>>", end='')
 	username = input()
 	password = getpass.getpass("Enter password (No text will show up for security of passowrd)\n>>")
 	return ({"username" : username, "password": password})
+# Enters information into fields on the service manager website and hits the login button
 def perform_login(driver, info):
 	# Enter info and login
 	driver.find_element_by_id("LoginUsername").send_keys(info["username"])
@@ -60,6 +66,9 @@ def perform_login(driver, info):
 	driver.find_element_by_id("loginBtn").click()
 	# allow page to load
 	time.sleep(10)
+# Opens PI Support group then every quote within that group
+# Each ticket opened is processed to see if it is a software request
+# Returns a dictionary with keys being quote number and values being dictionaries with spreadsheet info
 def open_and_process_tickets(driver):
 	soup = BeautifulSoup(driver.page_source, "html.parser")
 	# Find iframe ID
@@ -92,6 +101,8 @@ def open_and_process_tickets(driver):
 		ticketIDs = find_ticket_IDs(driver)
 		process_ticket(driver, ticketIDs[ticket], ticketsDict)
 	return ticketsDict
+# Processes description field of the ticket to see if it is a software request
+# Closes ticket after it is processed
 def process_ticket(driver, ticketID, ticketsDict):
 	# Click on ticket
 	driver.find_element_by_id(ticketID).click()
@@ -113,6 +124,8 @@ def process_ticket(driver, ticketID, ticketsDict):
 				cancelID = b["id"]
 	driver.find_element_by_id(cancelID).click()
 	time.sleep(5)
+# Looks through all tickets and checks if it is a quote
+# Returns list with html IDs of all quotes
 def find_ticket_IDs(driver):
 	ticketIDs = []
 	soup = BeautifulSoup(driver.page_source, "html.parser")
@@ -130,6 +143,8 @@ def find_ticket_IDs(driver):
 			if "Q" in ticket.string:
 				ticketIDs.append(ticket["id"])
 	return ticketIDs
+# Looks for key words to indicate that the ticket is for a software request
+# Returns boolean indicating if it's a process request
 def software_request(description):
 	isSoftwareRequest = False
 	if "processbook" in description or "process book" in description or "pi process" in description or "data link" in description or "datalink" in description or "pi data" in description:
@@ -139,7 +154,11 @@ def software_request(description):
 			isSoftwareRequest = True
 		elif "transfer" in description:
 			isSoftwareRequest = True
+		elif "software" in description:
+			isSoftwareRequest = True
 	return isSoftwareRequest
+# Collects necessary spreadsheet information of software request ticket
+# Populates the ticket dictionary in corresponding quote value
 def process_software_request(soup, description, ticketsDict):
 	# Collect parent Quote number
 	quoteTag = soup.find("input", {"name": "instance/parent.quote"})
@@ -148,6 +167,7 @@ def process_software_request(soup, description, ticketsDict):
 	ticketsDict[parentQuote] = {}
 	# Check if its a processbook request, data link request, or both
 	if "data link" in description or "datalink" in description or "pi data" in description:
+		# Fill out quote dictionary with data link info
 		ticketsDict[parentQuote]["data link filter"] = "Data Link 2013"
 		tag = find_tag(description)
 		ticketsDict[parentQuote]["data link tag"] = tag
@@ -159,25 +179,33 @@ def process_software_request(soup, description, ticketsDict):
 		ticketsDict[parentQuote]["data link SAP ID"] = sapID
 		ticketsDict[parentQuote]["request date"] = requestDate
 	if "processbook" in description or "process book" in description or "pi process" in description:
-		ticketsDict[parentQuote]["process book filter"] = "Win7 - ProcessBook"
+		# fill out quote dictionary with processbook info
+		ticketsDict[parentQuote]["processbook filter"] = "Win7 - ProcessBook"
 		tag = find_tag(description)
-		ticketsDict[parentQuote]["process book tag"] = tag
+		ticketsDict[parentQuote]["processbook tag"] = tag
 		oldTag = find_old_tag(description)
-		ticketsDict[parentQuote]["process book old tag"] = oldTag
+		ticketsDict[parentQuote]["processbook old tag"] = oldTag
 		client = find_client(soup)
-		ticketsDict[parentQuote]["process book client"] = client
+		ticketsDict[parentQuote]["processbook client"] = client
 		sapID = find_sap_id(soup)
-		ticketsDict[parentQuote]["process book SAP ID"] = sapID
+		ticketsDict[parentQuote]["processbook SAP ID"] = sapID
 		ticketsDict[parentQuote]["request date"] = requestDate
+# Finds and returns tag number (TAG######) of new machine which software will be installed
+# If no new tag is found the string "No tag found" is returned
+# If there is a python error in finding the tag the string "Error finding tag" is returned
 def find_tag(description):
 	tag = "No tag found"
 	whitespace = [" ", ",", "\n", "\t", ":", ";"]
 	try:
+		# find tag if only 1 tag is mentioned
 		if description.count("tag") == 1:
 			tag = ""
+			# find index of tag number
 			index = description.index("tag") + 3
+			# ignore whitespace
 			while index < len(description) and description[index] in whitespace:
 				index += 1
+			# collect numbers of tag
 			while  index < len(description) and description[index].isdecimal():
 				tag = tag + description[index]
 				index += 1
@@ -203,15 +231,21 @@ def find_tag(description):
 	except:
 		tag = "Error finding tag"
 	return tag
+# Finds and returns old tag number (TAG######) of machine that software was previously on
+# If no old tag is found the string "No old tag found" is returned
+# If there is a python error in finding the old tag the string "Error finding tag" is returned
 def find_old_tag(description):
 	oldTag = "No old tag found"
 	whitespace = [" ", ",", "\n", "\t", ":", ";"]
 	try:
 		if "old tag" in description:
 			oldTag = ""
+			# find index of tag number
 			index = description.index("old tag") + 7
+			# ignore whitespace
 			while index < len(description) and description[index] in whitespace:
 				index += 1
+			# collect number of tag
 			while  index < len(description) and description[index].isdecimal():
 				oldTag = oldTag + description[index]
 				index += 1
@@ -228,6 +262,7 @@ def find_old_tag(description):
 	except:
 		oldTag = "Error finding tag"
 	return oldTag
+# Finds and returns the name of the client requesting the software
 def find_client(soup):
 	client = "no client found"
 	tag = soup.find("input", {"id": "X26"})
@@ -236,6 +271,7 @@ def find_client(soup):
 	except:
 		pass
 	return client
+# Finds and returns the SAP ID of the client requesting the software
 def find_sap_id(soup):
 	sapID = "no SAP ID found"
 	tag = soup.find("input", {"id": "X24"})
@@ -244,9 +280,10 @@ def find_sap_id(soup):
 	except:
 		pass
 	return sapID
+# Fills out worksheet with information in the tickets dictionary
 def populate_worksheet(worksheet, ticketsDict):
 	quotes = ticketsDict.keys()
-	# Print all data link requests
+	# Print all data link request info in corresponding columns
 	row = 1
 	for quote in quotes:
 		if "data link filter" in ticketsDict[quote]:
@@ -258,15 +295,15 @@ def populate_worksheet(worksheet, ticketsDict):
 			worksheet.write_string(row, 5, ticketsDict[quote]["data link SAP ID"])
 			worksheet.write_string(row, 7, ticketsDict[quote]["request date"])
 			row += 1
-	# Print all process book requests
+	# Print all processbook request info in corresponding columns
 	for quote in quotes:
-		if "process book filter" in ticketsDict[quote]:
+		if "processbook filter" in ticketsDict[quote]:
 			worksheet.write_string(row, 0, quote)
-			worksheet.write_string(row, 1, ticketsDict[quote]["process book filter"])
-			worksheet.write_string(row, 2, ticketsDict[quote]["process book tag"])
-			worksheet.write_string(row, 3, ticketsDict[quote]["process book old tag"])
-			worksheet.write_string(row, 4, ticketsDict[quote]["process book client"])
-			worksheet.write_string(row, 5, ticketsDict[quote]["process book SAP ID"])
+			worksheet.write_string(row, 1, ticketsDict[quote]["processbook filter"])
+			worksheet.write_string(row, 2, ticketsDict[quote]["processbook tag"])
+			worksheet.write_string(row, 3, ticketsDict[quote]["processbook old tag"])
+			worksheet.write_string(row, 4, ticketsDict[quote]["processbook client"])
+			worksheet.write_string(row, 5, ticketsDict[quote]["processbook SAP ID"])
 			worksheet.write_string(row, 7, ticketsDict[quote]["request date"])
 			row += 1
 def main():
